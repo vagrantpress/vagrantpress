e0b9a8826b105b618c6d060134b844d8b40a3701
Adam Stokes <adam.stokes@ubuntu.com>
2013-06-18 11:38:33 -0400

    Add proper rewrite rules to vhost
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -2,29 +2,42 @@ class apache2::install{
   
   package { "apache2": ensure => present,}
 
-    service { "apache2":
-      ensure => running,
-      require => Package["apache2"],
-    }
+  service { "apache2":
+    ensure => running,
+    require => Package["apache2"],
+  }
 
-    exec{"apache-modrewrite": #tee hee
-      command=>"/usr/sbin/a2enmod rewrite",
-      cwd=>"/vagrant/",
-    }
+  file { '/etc/apache2/sites-available/default':
+    owner => root,
+    group => root,
+    ensure => file,
+    mode => 644,
+    source => '/vagrant/files/etc/apache2/sites-available/default',
+    require => Package["apache2"],
+  }
 
-    file { '/etc/apache2/sites-available/default':
-      owner => root,
-      group => root,
-      ensure => file,
-      mode => 644,
-      source => '/vagrant/files/etc/apache2/sites-available/default',
-      require => Package["apache2"],
-    }
+  file { '/etc/apache2/mods-available/rewrite.load':
+    owner => root,
+    group => root,
+    ensure => file,
+    mode => 644,
+    source => '/vagrant/files/etc/apache2/mods-available/rewrite.load',
+    require => Package['apache2'],
+  }
 
-    file { '/etc/apache2/sites-enabled/000-default':
-      notify => Service["apache2"],
-      ensure => link,
-      target => "/etc/apache2/sites-available/default",
-      require => Package["apache2"],
-    }
+
+  file { '/etc/apache2/sites-enabled/000-default':
+    notify => Service["apache2"],
+    ensure => link,
+    target => "/etc/apache2/sites-available/default",
+    require => Package["apache2"],
+  }
+
+  file { '/etc/apache2/mods-enabled/rewrite.load':
+    notify => Service["apache2"],
+    ensure => link,
+    target => "/etc/apache2/mods-available/rewrite.load",
+    require => Package["apache2"],
+  }
+  
 }
