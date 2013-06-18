5d5c8016aa01eb716803ddadbddd2216c75ca74c
Adam Stokes <adam.stokes@ubuntu.com>
2013-06-18 01:36:13 -0400

    add default config
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -7,10 +7,4 @@ class apache2::install{
       require => Package["apache2"],
     }
 
-    file { '/var/www':
-      ensure => link,
-      target => "/vagrant",
-      notify => Service['apache2'],
-      force  => true
-    }
   }
--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -1,5 +1,8 @@
 class php5::install{
   package{"php5": ensure=>present,}
   package{"php5-mysql": ensure=>present,}
+  package{"php5-curl": ensure=>present,}
+  package{"php5-gd": ensure=>present,}
+  package{"php5-fpm": ensure=>present,}
   package{"libapache2-mod-php5": ensure=>present,}
 }
