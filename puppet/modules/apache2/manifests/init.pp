3f6579b676226098ee244085e6217bcc791a27dd
bchhun <bernard.chhun@gmail.com>
2013-06-26 17:46:08 -0400

    Issue #2: added a httpd.conf file that changes the "user|group" that is used to run apache2 to "vagrant|vagrant" instead of "www-data|www-data"

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -1,10 +1,22 @@
 class apache2::install{
-  
+
   package { "apache2": ensure => present,}
 
   service { "apache2":
     ensure => running,
     require => Package["apache2"],
+  }
+
+  /*
+    the httpd.conf change the user/group that apache uses to run its process
+   */
+  file { '/etc/apache2/httpd.conf':
+    owner => root,
+    group => root,
+    ensure => file,
+    mode => 644,
+    source => '/vagrant/files/etc/apache2/httpd.conf',
+    require => Package["apache2"]
   }
 
   file { '/etc/apache2/sites-available/default':
@@ -39,5 +51,5 @@ class apache2::install{
     target => "/etc/apache2/mods-available/rewrite.load",
     require => Package["apache2"],
   }
-  
+
 }
