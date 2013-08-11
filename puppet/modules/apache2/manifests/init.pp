43cc377c4f4f8dbd6bd28db351260bea928d010e
Stuart Grimshaw <stuart@localphone.com>
2013-08-11 21:58:30 +0100

    Made sure apache is restarted when user is changed to Vagrant

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -10,13 +10,14 @@ class apache2::install{
   /*
     the httpd.conf change the user/group that apache uses to run its process
    */
-  file { '/etc/apache2/httpd.conf':
+  file { '/etc/apache2/conf.d/user':
     owner => root,
     group => root,
     ensure => file,
     mode => 644,
     source => '/vagrant/files/etc/apache2/httpd.conf',
-    require => Package["apache2"]
+    require => Package["apache2"],
+    notify  => Service['apache2']
   }
 
   file { '/etc/apache2/sites-available/default':
@@ -26,6 +27,7 @@ class apache2::install{
     mode => 644,
     source => '/vagrant/files/etc/apache2/sites-available/default',
     require => Package["apache2"],
+    notify  => Service['apache2']
   }
 
   file { '/etc/apache2/mods-available/rewrite.load':
@@ -35,6 +37,7 @@ class apache2::install{
     mode => 644,
     source => '/vagrant/files/etc/apache2/mods-available/rewrite.load',
     require => Package['apache2'],
+    notify  => Service['apache2']
   }
 
 
