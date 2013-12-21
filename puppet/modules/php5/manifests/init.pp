e0ca8ad44cf0216921021cdd569462e9086dbc0b
Daryl Lozupone <daryl@clubduece.com>
2013-12-21 08:49:16 -0500

    ADD: required packages for phpqa Install WP codesniffs for phpcs

--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -2,15 +2,53 @@
 
 class php5::install {
 
-  package { [
-      'php5',
-      'php5-mysql',
-      'php5-curl',
-      'php5-gd',
-      'php5-fpm',
-      'libapache2-mod-php5',
-    ]:
-    ensure => present,
-  }
-
+	package { [
+	  'php5',
+	  'php5-mysql',
+	  'php5-curl',
+	  'php5-gd',
+	  'php5-fpm',
+	  'libapache2-mod-php5',
+	  'php5-dev',
+	  'php5-xdebug',
+	  'php-pear',
+	]:
+	ensure => present,
+	}
+  
+	# upgrade pear
+	exec {"pear upgrade":
+	  command => "/usr/bin/pear upgrade",
+	  require => Package['php-pear'],
+	}
+	
+	# set channels to auto discover
+	exec { "pear auto_discover" :
+		command => "/usr/bin/pear config-set auto_discover 1",
+		require => [Package['php-pear']]
+	}
+	
+	# update channels
+	exec { "pear update-channels" :
+		command => "/usr/bin/pear update-channels",
+		require => [Package['php-pear']]
+	}
+	
+	# install Xdebug
+	exec { "Install Xdebug" :
+		command => "/usr/bin/pecl install xdebug",
+		require => [Package['php5-dev']]
+	}
+	
+	# Set development values to our php.ini and xdebug.ini
+	augeas { 'set-php-ini-values':
+	    context => '/files/etc/php5/apache2/php.ini',
+	    changes => [
+	        'set PHP/error_reporting "E_ALL | E_STRICT"',
+	        'set PHP/display_errors On',
+	        'set PHP/display_startup_errors On',
+	        'set PHP/html_errors On',
+	     ],
+	     require => Package['apache2', 'augeas-tools']
+	}
 }
--- puppet/modules/phpqa/manifests/init.pp
+++ puppet/modules/phpqa/manifests/init.pp
@@ -35,6 +35,13 @@ class phpqa::install{
 	  require => Exec['pear update-channels']
 	}
 	
+	# install WordPress Coding Standard sniffs
+	# http:////github.com/WordPress-Coding-Standards/WordPress-Coding-Standards
+	exec { "Install WP phpcs sniffs":
+		command => "/usr/bin/git clone git://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git $(pear config-get php_dir)/PHP/CodeSniffer/Standards/WordPress",
+		require => Exec['pear update-channels', 'pear install phpcs']
+	}
+	
 	# install phpdepend
 	exec {"pear install pdepend":
 	  command => "/usr/bin/pear install --alldeps pear.pdepend.org/PHP_Depend-beta",
