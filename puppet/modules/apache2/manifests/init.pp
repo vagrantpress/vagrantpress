96fc2236604309bfaf2db162429efa3cdbb09444
Rob Terhaar <robbyt@robbyt.net>
2014-09-13 20:15:40 -0400

    Use resource defaults to reduce duplication

--- puppet/modules/apache2/manifests/init.pp
+++ puppet/modules/apache2/manifests/init.pp
@@ -2,62 +2,49 @@
 
 class apache2::install {
 
-  package { 'apache2':
-    ensure => present,
-  }
-
-  service { 'apache2':
-    ensure  => running,
-    require => Package['apache2'],
-  }
-
-  # the httpd.conf change the user/group that apache uses to run its process
-  file { '/etc/apache2/conf-available/user.conf':
+  File {
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    source  => '/vagrant/files/etc/apache2/httpd.conf',
     require => Package['apache2'],
     notify  => Service['apache2'],
   }
+  
+  package { 'apache2':
+    ensure => present,
+  }->
+
+  service { 'apache2':
+    ensure  => running,
+  }
+
+  # the httpd.conf change the user/group that apache uses to run its process
+  file { '/etc/apache2/conf-available/user.conf':
+    source  => '/vagrant/files/etc/apache2/httpd.conf',
+  }
+  
   file { '/etc/apache2/conf-enabled/user.conf':
     ensure => link,
     target => '/etc/apache2/conf-available/user.conf',
   }
 
   file { '/etc/apache2/sites-available/default.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => '/vagrant/files/etc/apache2/sites-available/default.conf',
-    require => Package['apache2'],
-    notify  => Service['apache2']
   }
 
   file { '/etc/apache2/mods-available/rewrite.load':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => '/vagrant/files/etc/apache2/mods-available/rewrite.load',
-    require => Package['apache2'],
-    notify  => Service['apache2']
   }
 
   file { '/etc/apache2/sites-enabled/000-default.conf':
     ensure  => link,
     target  => '/etc/apache2/sites-available/default.conf',
-    require => Package['apache2'],
-    notify  => Service['apache2'],
   }
 
   file { '/etc/apache2/mods-enabled/rewrite.load':
     ensure  => link,
     target  => '/etc/apache2/mods-available/rewrite.load',
-    require => Package['apache2'],
-    notify  => Service['apache2'],
   }
 
 }
